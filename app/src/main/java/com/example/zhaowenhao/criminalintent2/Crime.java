07fa5579c4f7566fb2d781231f5f8c018981bce7
zhao wenhao <zhaowenhao@outlook.com>
2016-04-06 00:11:57 +0800

    chapter 17: mostly done, public Crime(JSONObject json) in Crime.java needs to be fixed later

--- app/src/main/java/com/example/zhaowenhao/criminalintent2/Crime.java
+++ app/src/main/java/com/example/zhaowenhao/criminalintent2/Crime.java
@@ -1,5 +1,8 @@
 package com.example.zhaowenhao.criminalintent2;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Date;
 import java.util.UUID;
 
@@ -11,6 +14,11 @@ public class Crime {
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
+
+    private static final String JSON_ID = "id";
+    private static final String JSON_TITLE = "title";
+    private static final String JSON_SOLVED = "solved";
+    private static final String JSON_DATE = "date";
 
     public Crime(){
         mID = UUID.randomUUID();
@@ -48,5 +56,24 @@ public class Crime {
     @Override
     public String toString(){
         return  mTitle;
+    }
+
+    public JSONObject toJSON() throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put(JSON_ID, mID.toString());
+        json.put(JSON_TITLE, mTitle);
+        json.put(JSON_DATE, mDate.getTime());
+        json.put(JSON_SOLVED, mSolved);
+
+        return json;
+    }
+
+    public Crime(JSONObject json) throws JSONException{
+        mID = UUID.fromString(json.getString(JSON_ID));
+        if(json.has(JSON_TITLE)){
+            mTitle = json.getString(JSON_TITLE);
+        }
+        mSolved = json.getBoolean(JSON_SOLVED);
+        mDate = new Date(); /// ??? need to fix
     }
 }
